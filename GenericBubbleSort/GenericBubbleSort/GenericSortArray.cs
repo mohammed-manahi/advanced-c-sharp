c01bc6b8904023774d17a81d92da92d8f0595bc1
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-01-22 20:13:54 +0300

    Generic bubble sort

---
+++ GenericBubbleSort/GenericBubbleSort/GenericSortArray.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericBubbleSort;
+
+public class GenericSortArray<T> where T: IComparable
+{
+    // T type requires constraint IComparable to compare array items
+    public void BubbleSort(T[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j].CompareTo(array[j + 1]) > 0)
+                {
+                    Swap(array, j);
+                }
+            }
+        }
+        Console.WriteLine(string.Join("," , array));
+    }
+
+    private void Swap(T[] array, int j)
+    {
+        T temp = array[j];
+        array[j] = array[j +1];
+        array[j + 1] = temp;
+    }
+}
---
+++ GenericBubbleSort/GenericBubbleSort/Program.cs
@@ -0,0 +1,10 @@
+﻿using GenericBubbleSort;
+
+object[] numbersArray = new object[] { 1, 4, 7, 3, 9, 5, 0 };
+string[] stringsArray = new string[] { "Mahmoud", "Ali", "Ahmed", "Mohammed", "Hasan"};
+SortArray sortArray = new SortArray();
+sortArray.BubbleSort(numbersArray);
+sortArray.BubbleSort(stringsArray);
+
+GenericSortArray<string> genericSortArray = new GenericSortArray<string>();
+genericSortArray.BubbleSort(stringsArray);
