9998eeb143206d489e6a27b6c285c326a4e4b23c
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-01-22 13:52:22 +0300

    Generics basic example

---
+++ GenericsBasicExample/GenericsBasicExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using GenericsBasicExample;
+
+Salaries salaries = new Salaries();
+
+List<float> salariesList = salaries.GetSalaries();
+
+float salary = (float)salariesList[1];
+
+salary += salary * 0.02f;
+
+Console.WriteLine(salary);
---
+++ GenericsBasicExample/GenericsBasicExample/Salaries.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace GenericsBasicExample;
+
+public class Salaries
+{
+    // ArrayList is not a generic collection while List is a generic collection
+    // ArrayList typically stores an object data type while List stores user-specified data type for T 
+    // Generic collections such as list solves the problem of boxing and unboxing of object type in non-generic collections such as ArrayList
+    
+    private List<float> _salariesList = new List<float>();
+
+    public Salaries()
+    {
+        _salariesList.Add(5000.53f);
+        _salariesList.Add(4000.62f);
+        _salariesList.Add(6000.13f);
+    }
+
+    public List<float> GetSalaries()
+    {
+        return _salariesList;
+    }
+}
