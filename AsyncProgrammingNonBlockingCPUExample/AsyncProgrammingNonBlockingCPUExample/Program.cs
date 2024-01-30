b03708480e235f5b30a8fa3a40ac926ac68b2ed3
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-01-30 21:31:27 +0300

    Implement concurrent tasks

---
+++ AsyncProgrammingNonBlockingCPUExample/AsyncProgrammingNonBlockingCPUExample/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace AsyncProgrammingNonBlockingCPUExample;
+
+// A non-blocking cpu task implementation using TAP approach
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Method Name: {nameof(Main)}, Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        StockMarketTechnicalAnalysisData stockMarketTechnicalAnalysisData =
+            new StockMarketTechnicalAnalysisData(
+                "STKZA",
+                new DateTime(2010, 1, 1),
+                new DateTime(2020, 1, 1));
+        
+        DateTime before = DateTime.Now;
+        // Synchronous implementation
+        // decimal[] data1 = stockMarketTechnicalAnalysisData.GetOpeningPrices();
+        // decimal[] data2 = stockMarketTechnicalAnalysisData.GetClosingPrices();
+        // decimal[] data3 = stockMarketTechnicalAnalysisData.GetPriceHighs();
+        // decimal[] data4 = stockMarketTechnicalAnalysisData.GetPriceLows();
+        
+        // Asynchronous implementation
+        List<Task<decimal[]>> tasks = new List<Task<decimal[]>>();
+        
+        Task<decimal[]> openingPricesTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetOpeningPrices());
+        Task<decimal[]> closingPricesTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetClosingPrices());
+        Task<decimal[]> priceHighsTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetPriceHighs());
+        Task<decimal[]> priceLowsTask = Task.Run(() => stockMarketTechnicalAnalysisData.GetPriceLows());
+        
+        tasks.Add(openingPricesTask);
+        tasks.Add(closingPricesTask);
+        tasks.Add(priceLowsTask);
+        tasks.Add(openingPricesTask);
+        // Wait for all four methods to be completed
+        Task.WaitAll(tasks.ToArray());
+        decimal[] data1 = tasks[0].Result;
+        decimal[] data2 = tasks[1].Result;
+        decimal[] data3 = tasks[2].Result;
+        decimal[] data4 = tasks[3].Result;
+        
+        DateTime after = DateTime.Now;
+        TimeSpan duration = after.Subtract(before);
+        Console.WriteLine($"Time duration: {duration.Seconds} {(duration.Seconds >1 ? "seconds" : "second")}");
+        // decimal[] data5 = stockMarketTechnicalAnalysisData.CalculteStochastics();
+        // decimal[] data6 = stockMarketTechnicalAnalysisData.CalculateFastMovingAverage();
+        // decimal[] data7 = stockMarketTechnicalAnalysisData.CalculateSlowMovingAverage();
+        // decimal[] data8 = stockMarketTechnicalAnalysisData.CalculateUpperBoundBollingerBand();
+        // decimal[] data9 = stockMarketTechnicalAnalysisData.CalculateLowerBoundBollingerBand();
+    }
+}
---
+++ AsyncProgrammingNonBlockingCPUExample/AsyncProgrammingNonBlockingCPUExample/StockMarketTechnicalAnalysisData.cs
@@ -0,0 +1,83 @@
+﻿namespace AsyncProgrammingNonBlockingCPUExample;
+
+public class StockMarketTechnicalAnalysisData
+{
+    public StockMarketTechnicalAnalysisData(string stockSymbol, DateTime startDateTime, DateTime endDateTime)
+    {
+        // Call remote server to get analysis data
+    }
+
+    public decimal[] GetOpeningPrices()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(GetOpeningPrices)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] GetClosingPrices()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(GetClosingPrices)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] GetPriceHighs()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(GetPriceHighs)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] GetPriceLows()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(GetPriceLows)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] CalculteStochastics()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(CalculteStochastics)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(10000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] CalculateFastMovingAverage()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(CalculateFastMovingAverage)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(6000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] CalculateSlowMovingAverage()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(CalculateSlowMovingAverage)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(7000);
+        data = new decimal[]{};
+        return data;
+    }
+    public decimal[] CalculateUpperBoundBollingerBand()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(GetOpeningPrices)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(5000);
+        data = new decimal[]{};
+        return data;
+    }
+    
+    public decimal[] CalculateLowerBoundBollingerBand()
+    {
+        decimal[] data;
+        Console.WriteLine($"Method name: {nameof(CalculateLowerBoundBollingerBand)}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(5000);
+        data = new decimal[]{};
+        return data;
+    }
+}
